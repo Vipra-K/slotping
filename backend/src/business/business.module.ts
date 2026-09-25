d005c5d58330f23e845ff7a910bf38685577f750
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-09-25 12:17:16 +0530

    feat: add core NestJS modules

---
+++ backend/src/business/business.module.ts
@@ -0,0 +1,5 @@
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Business} from './business.entity';
+@Module({imports:[TypeOrmModule.forFeature([Business])],exports:[TypeOrmModule]})
+export class BusinessModule{}
